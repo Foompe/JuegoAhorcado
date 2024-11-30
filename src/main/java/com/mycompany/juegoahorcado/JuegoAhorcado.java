c96a17fea4926c0c505aef5cd85104229d3051a5
Rubén Fompedriña <foompe@gmail.com>
2024-11-30 21:41:16 +0100

    Creado estructura inicial de juego, creado metodo contadorFallos

--- src/main/java/com/mycompany/juegoahorcado/JuegoAhorcado.java
+++ src/main/java/com/mycompany/juegoahorcado/JuegoAhorcado.java
@@ -8,25 +8,62 @@ import java.util.Scanner;
 /**
  *
  * @author FP Multip
- * 
+ *
  * Objetivo, Hacer un juego de tres en raya que soporte como maximo 5 fallos
  */
 public class JuegoAhorcado {
 
     public static void main(String[] args) {
+        //Declaramos e iniciamos las variables
+        boolean continuar = true;
+        boolean fallosFin = true;
+        int fallo = 0;
 
-        int fallos = 0;
-       
+        Scanner sc = new Scanner(System.in);
+
+        while (continuar) {
+            System.out.println("Bienvenido al ahorcado!! \n"
+                    + "Selecciona una opcion:\n  (1) Jugar\n  (2) Salir");
+            String eleccion = sc.nextLine();
+            char elec = eleccion.charAt(0);
+
+            switch (elec) {
+                case '1':
+                    System.out.println("Has escogido Jugar");
+                    
+                    while (fallosFin) {
+                        
+                    //pedimos la palabra a adivinar
+                    System.out.println("Introduce palabra a adivinar");
+                    String palabra = sc.nextLine().toLowerCase();
+                    System.out.println("Has escogido: " + palabra);
+                    
+                    //cambiamos los valores de la palabra por incognitas: "x"
+                    
+                    
+                    fallosFin = contadorFallos(fallo);
+                    }
+                    break;
+                case '2':
+                    System.out.println("Has escogido Salir");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Elección erronea, escoge de nuevo");
+            }
+
+        }
+
     }
 
-
     /**
-     * 
+     *
      * @param fallo toma el número de fallos e imprime un mensaje en pantalla
      */
-    public void impresionFallos(int fallo) {
-        
-        int fallos =fallo;
+    public static boolean contadorFallos(int fallo) {
+
+        //introducir un contador de fallos
+        int fallos = fallo;
         
         switch (fallos) {
             case 0:
@@ -34,7 +71,7 @@ public class JuegoAhorcado {
                 System.out.println(".|...|.");
                 System.out.println(".O...|.");
                 System.out.println("-|-..|.");
-                System.out.println("/.\\.|.");
+                System.out.println("/.\\..|.");
                 System.out.println(".....|.");
                 System.out.println("-------");
                 break;
@@ -84,5 +121,9 @@ public class JuegoAhorcado {
                 System.out.println("-------");
                 break;
         }
+        if (fallos >= 5) {
+            return false;
+        }
+        return true;
     }
 }
